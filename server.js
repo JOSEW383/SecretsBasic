3f05c43ff531d9c06c04f975c00233ccaaf6313e
Josew383 <josew383@gmail.com>
2023-02-06 22:31:37 +0100

    Fix: added DOMAN variable

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets"
+  callbackURL: process.env.DOMAIN+"/auth/google/secrets"
 },
 function(accessToken, refreshToken, profile, cb) {
   // console.log(profile);
@@ -66,5 +66,5 @@ function(accessToken, refreshToken, profile, cb) {
 db.main().catch(err => console.log(err));
 routes.setRoutes(app, db, passport, faker);
 app.listen(port, function() {
-  console.log("Server listening at http://localhost:"+port);
+  console.log("Server listening at "+process.env.DOMAIN+":"+port);
 });
